888512783c8951c993fc3c6494bbcfed7af9ea36
Blackmoon <dwydler@users.noreply.github.com>
2026-06-22 20:10:52 +0200

    Add error handling for SBOM generation in Generate-SBOM.sh (#399)

--- images/ubuntu/scripts/docs-gen/Generate-SBOM.sh
+++ images/ubuntu/scripts/docs-gen/Generate-SBOM.sh
@@ -10,8 +10,20 @@ OutputDirectory="$1"
 # Install sysft
 curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | sh -s -- -b /usr/local/bin
 
-# Generating SBOM 
-syft dir:/ -o spdx-json=$OutputDirectory/sbom.json
+# Generating SBOM
+if ! syft dir:/ -o spdx-json="$OutputDirectory/sbom.json"; then
+    echo ""
+    echo "ERROR: SBOM generation failed."
+
+    if dmesg | grep -qi -E "killed process|out of memory"; then
+        echo "Reason: The operating system killed Syft because of insufficient memory (OOM)."
+        echo "Syft was likely using too much RAM while scanning '/'."
+    else
+        echo "Reason: Syft returned an unexpected error."
+    fi
+
+    exit 1
+fi
 
 # Preparing artifact (raw SBOM.json is too big)
 cd $OutputDirectory
